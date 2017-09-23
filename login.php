ad12cfa678e94da8cd26a3672f04b1471cb8ba58
KABarnachea <31788551+KABarnachea@users.noreply.github.com>
2017-09-23 14:55:40 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php	
+	if(isset($_POST["login"])){
+
+		$con = new PDO("mysql:host=localhost; dbname=db_users;","root","");
+
+		$sql="SELECT * FROM uuser WHERE uname='". $_POST["txtuname"] ."' and password='". $_POST["txtpword"] ."'";
+			$result=$con->query($sql); 
+			$rows=$result->rowCount(); 
+			if($rows>=1){
+				header("location: profile.php");
+			}else{
+				echo "Invalid username and password combination";
+			}
+
+	}
+
+?>
+
+<?php	
+	if(isset($_POST["signup"])){
+
+		// create a connection, just like in basic login
+		$con = new PDO("mysql:host=localhost; dbname=db_users;","root","");
+
+		// INSERT INTO is ansql command to add new record to the databse
+		$sql="INSERT INTO uuser VALUES('". $_POST["txtuname"] ."', '". $_POST["txtpword"] ."')";
+		$con->query($sql); //the command will be executed and a new record will be added to the databse
+		echo "New Record Saved!";
+	}
+
+?>
---
+++ profile.php
@@ -0,0 +1,49 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+		
+		<title>Sample Login and Sign up</title>
+
+		<!-- Loading third party fonts -->
+		<link href="http://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
+		<link href="fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+		<!-- Loading main css file -->
+		<link rel="stylesheet" href="style.css">
+		
+		<!--[if lt IE 9]>
+		<script src="js/ie-support/html5.js"></script>
+		<script src="js/ie-support/respond.js"></script>
+		<![endif]-->
+
+	</head>
+
+<body>
+	<main class="main-content">
+				<div class="fullwidth-block">
+					<div class="container">
+						<h2 class="section-title"><?php
+
+						echo "Welcome to profile";
+						?></h2>
+						<small class="section-subtitle">Kylyn Angel Barnachea</small>
+						
+
+						<img src="images/devices.png" alt="" class="figure" style="height:300px;width:550px;">
+					</div>
+				</div> <!-- .fullwidth-block -->
+
+			<script src="js/jquery-1.11.1.min.js"></script>
+		<script src="js/plugins.js"></script>
+		<script src="js/app.js"></script>
+	</body>
+
+</html>
+		
+		
+
